3eed621d484f2b890c0c8fad78fad4394255d13c
RoshanDalami <roshandalami0@gmail.com>
2023-09-27 21:13:09 +0545

    fixing banner part

--- src/Components/BlogHero.jsx
+++ src/Components/BlogHero.jsx
@@ -8,9 +8,9 @@ export default function BlogHero(){
             <div className="m-10 flex items-end " >
                
                 <div className=" absolute text-white flex flex-col gap-3   justify-end  py-14 px-10   ">
-                    <p className="text-[20px]">06-04-2023</p>
+                    <p className="text-[20px]">Abroad Sathi | 06-04-2023</p>
                     <h1 className="text-[28px]">How to Choose the Right College Major: Tips and Advice</h1>
-                    <button className="text-[20px] font-semibold rounded-full bg-transparent border-2 border-white px-3 py-1 text-white w-[250px]">LEARN MORE</button>
+
                 </div>
                 <Image src={banner} alt="saminar" className=""   />
             </div>
