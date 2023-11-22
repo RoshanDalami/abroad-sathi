8e7941c3aebec336021298558cb05f7af5dd03c4
RoshanDalami <roshandalami0@gmail.com>
2023-11-22 22:04:18 +0545

    changed nav items active class from navlink to clsx classes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,14 +7,19 @@ import { useRouter } from "next/navigation";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { NavLink } from "./NavLink";
 
+
 //component imports
 import Button from "./Button";
 import Logo from "@/Images/logo.png";
-import { useState } from "react";
+import { useState , useLayoutEffect  } from "react";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const router = useRouter();
   const [isMobile, setIsMobile] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const pathname = usePathname();
   const onHomeClick = () => {
     router.push("/");
     setTimeout(() => {
@@ -28,7 +33,10 @@ export default function Navbar() {
     }, 500);
   };
   const onServiceClick = () => {
-    router.push("/#services");
+    window.scrollTo({
+      top: 3100,
+      behavior: "smooth",
+    });
     setTimeout(() => {
       setIsMobile(false);
     }, 500);
@@ -42,6 +50,42 @@ export default function Navbar() {
   const toggle = () => {
     setIsMobile((prevState) => !prevState);
   };
+  useLayoutEffect(() => {
+    function updateScrollPosition() {
+      setScrollPosition(window.pageYOffset);
+    }
+    // Add an event listener to update the scroll position on scroll
+    window.addEventListener("scroll", updateScrollPosition);
+
+    // Call the update function initially to get the initial scroll position
+    updateScrollPosition();
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", updateScrollPosition);
+    };
+  }, []);
+ 
+  const handleService = () => {
+    window.scrollTo({
+      top: 990,
+      behavior: "smooth",
+    });
+  };
+  
+
+  const handleHome = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  const navigation = [
+    { name: "HOME", href: "/", },
+    { name: "ABOUT", href: "/about" },
+    { name: "SERVICES", href: "/#services " },
+    { name: "BLOGS", href: "/blogs"},
+  ];
   return (
     <>
       <div>
@@ -53,17 +97,12 @@ export default function Navbar() {
           </section>
           <section className="hidden md:block">
             <ul className="flex gap-7 text-[#0F42AB]">
-              {[
-                ["HOME", onHomeClick,'/'],
-                ["ABOUT", onAboutClick,'/about'],
-                ["SERVICES", onServiceClick,'/#services'],
-                ["BLOGS", onBlogClick,'/blogs'],
-              ].map(([title, url,links]) => (
-                <NavLink href={links} key={nanoid()}>
-                  <li  className=" cursor-pointer">
-                    {title}
+              {navigation.map((item) => (
+                <Link href={item.href} key={nanoid()}>
+                  <li  className={clsx(" cursor-pointer",{'text-red-600':item.href === pathname})} onClick={item.onclick} >
+                    {item.name}
                   </li>
-                </NavLink>
+                </Link>
               ))}
             </ul>
           </section>
@@ -84,15 +123,12 @@ export default function Navbar() {
         {isMobile ? (
           <div className="flex flex-col gap-4 items-end px-10 justify-center ">
             <ul className="flex flex-col gap-7 text-[#0F42AB]">
-              {[
-                ["HOME", onHomeClick],
-                ["ABOUT", onAboutClick],
-                ["SERVICES", onServiceClick],
-                ["BLOGS", onBlogClick],
-              ].map(([title, url]) => (
-                <li onClick={url} key={nanoid()} className=" cursor-pointer">
-                  {title}
+              {navigation.map((item) => (
+                <Link href={item.href} key={nanoid()}>
+                <li  key={nanoid()} className=" cursor-pointer">
+                  {item.name}
                 </li>
+                </Link>
               ))}
             </ul>
             <section className="">
